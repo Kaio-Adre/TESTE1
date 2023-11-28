a75f544a97dc84e834e1df54fb1e374674ff020b
Kaio-Adré <andrebandeirakaio@gmail.com>
2023-11-28 16:33:24 -0300

    Commit - 101

---
+++ clinicasenac.sql
@@ -0,0 +1,7 @@
+SELECT * FROM clinicasenac.tb_consultas;
+SELECT * FROM clinicasenac.tb_pacientes;
+SELECT * FROM clinicasenac.tb_funcionarios;
+SELECT * FROM clinicasenac.tb_medicos;
+
+
+insert into tb_consultas(fk_idfun,fk_idmed, fk_idpac,obs)values(3,4,4,'TESTANDO');
